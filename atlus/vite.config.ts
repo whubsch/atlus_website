c20b2cdbd6f63342a1067f1b5e684f6b0e635253
Will <wahubsch@gmail.com>
2026-01-13 17:11:45 -0500

    fixing vite.config.ts issue

--- atlus/vite.config.ts
+++ atlus/vite.config.ts
@@ -4,14 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  return {
-    server: {
-      host: true,
-      port: 5173,
-      watch: {
-        usePolling: true,
-      },
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
     },
-    plugins: [react(), tailwindcss()],
-  };
+  },
+  plugins: [react(), tailwindcss()],
 });
